54b508fa1257319c6418a3432b0e4a1cc02e3100
Callum O'Neill <callumon@well.ox.ac.uk>
2024-09-05 13:18:10 +0100

    Ensuring that users install non-CRAN (bioconductor) ggmanh dependency

--- R/zzz.R
+++ R/zzz.R
@@ -20,3 +20,16 @@
   packageStartupMessage("Thank you for using MiamiR! Citation not required, but greatly appreciated :)")
   packageStartupMessage("Visit Github for requests, feedback, and info, and the www.theCRONproject.com for more data visualisation examples.")
 }
+
+
+#Need non-CRAN ggmanh first
+
+.onLoad <- function(libname, pkgname) {
+  if (!requireNamespace("ggmanh", quietly = TRUE)) {
+    message("ggmanh package not found. Installing it from Bioconductor...")
+    if (!requireNamespace("BiocManager", quietly = TRUE)) {
+      install.packages("BiocManager")
+    }
+    BiocManager::install("ggmanh")
+  }
+}
