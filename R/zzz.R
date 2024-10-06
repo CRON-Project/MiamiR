6de677276e682a0411141d3abba4c64f3cae1199
Callum O'Neill <callumon@well.ox.ac.uk>
2024-10-06 19:30:34 +0100

    View Rmarkdown in GitHub

--- R/zzz.R
+++ R/zzz.R
@@ -30,22 +30,22 @@
 
 #Need non-CRAN ggmanh first
 
-.onLoad <- function(libname, pkgname) {
-  if (!requireNamespace("BiocManager", quietly = TRUE)) {
-    install.packages("BiocManager")
-  }
-  if (!requireNamespace("ggmanh", quietly = TRUE)) {
-    message("ggmanh package not found. Installing it from Bioconductor...")
-    BiocManager::install("ggmanh")
-  }
-}
-
-.onLoad <- function(libname, pkgname) {
-  if (!requireNamespace("ggmanh", quietly = TRUE)) {
-    message("ggmanh package not found. Installing it from Bioconductor...")
-    if (!requireNamespace("BiocManager", quietly = TRUE)) {
-      install.packages("BiocManager")
-    }
-    BiocManager::install("ggmanh")
-  }
-}
+# .onLoad <- function(libname, pkgname) {
+#   if (!requireNamespace("BiocManager", quietly = TRUE)) {
+#     install.packages("BiocManager")
+#   }
+#   if (!requireNamespace("ggmanh", quietly = TRUE)) {
+#     message("ggmanh package not found. Installing it from Bioconductor...")
+#     BiocManager::install("ggmanh")
+#   }
+# }
+#
+# .onLoad <- function(libname, pkgname) {
+#   if (!requireNamespace("ggmanh", quietly = TRUE)) {
+#     message("ggmanh package not found. Installing it from Bioconductor...")
+#     if (!requireNamespace("BiocManager", quietly = TRUE)) {
+#       install.packages("BiocManager")
+#     }
+#     BiocManager::install("ggmanh")
+#   }
+# }
