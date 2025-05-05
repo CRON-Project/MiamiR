cb02e0c1283ab6fb9553c7364b835a45f27e4a7f
Callum O'Neill <callumon@well.ox.ac.uk>
2025-05-05 19:50:53 +0100

    er

--- R/Forest_Plot.R
+++ R/Forest_Plot.R
@@ -3311,7 +3311,7 @@ p <- res |>
 
       #  labels <- gsub("(?<=e)f", "╴⎯─", labels, perl = TRUE)  # U+2212 = −
 
-        labels <- gsub("(?<=e)f", "¯", labels, perl = TRUE)  # U+2212 = −
+        labels <- gsub("(?<=e)f", "¯ˍˉ", labels, perl = TRUE)  # U+2212 = −
 
         # Step 5: Final invisible formatting replacements
         formatted_labels <- gsub("Z", "<span style='color:#ffffff00;'>Z</span>", labels)
